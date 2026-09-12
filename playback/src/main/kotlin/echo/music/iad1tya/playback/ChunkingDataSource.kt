e358b3788226885ecdf193afce4ba57e50773d0a
Dharan Sundarapandiyan <dharan-25486@dharan-pt8014.csez.zohocorpin.com>
2026-09-13 03:24:24 +0530

    fix(playback): use DataSourceException.isPositionOutOfRange helper in ChunkingDataSource

--- playback/src/main/kotlin/echo/music/iad1tya/playback/ChunkingDataSource.kt
+++ playback/src/main/kotlin/echo/music/iad1tya/playback/ChunkingDataSource.kt
@@ -75,7 +75,7 @@ class ChunkingDataSource(
                 }
                 throw e
             } catch (e: androidx.media3.datasource.DataSourceException) {
-                if (e.reason == androidx.media3.datasource.DataSourceException.POSITION_OUT_OF_RANGE) {
+                if (androidx.media3.datasource.DataSourceException.isPositionOutOfRange(e)) {
                     return C.RESULT_END_OF_INPUT
                 }
                 throw e
